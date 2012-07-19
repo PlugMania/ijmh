f17cef8a9d52b0cc6d925613a73f1777d6ff527f
nsordk <nicolai@nsor.dk>
2012-07-19 21:19:19 +0200

    Cuncussion in fall added

--- src/info/plugmania/ijmh/effects/PlayerEffects.java
+++ src/info/plugmania/ijmh/effects/PlayerEffects.java
@@ -5,7 +5,10 @@ import java.util.Date;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
@@ -17,7 +20,7 @@ import info.plugmania.ijmh.ijmh;
 public class PlayerEffects {
 	
 	ijmh plugin;
-	public static String[] effects = new String[5 + 1];
+	public static String[] effects = new String[6 + 1];
 	static int effect;
 	public static long StruckTime = 0;
 
@@ -34,27 +37,29 @@ public class PlayerEffects {
 		effects[4] = ChatColor.AQUA + "You feel better! You where lucky this time.";
 	    // STRUCK BY LIGHTNING UNDER A TREE
 		effects[5] = ChatColor.RED + "Struck by lightning, didn't your mom teach you not to hide under trees during a storm!?";
+		// CUNCUSSION FROM FALL
+		effects[6] = ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC + "You might have hit the ground a bit too hard there ...";
 	}
 	
-	public static void addEffectInteract(int itemId, PlayerInteractEvent event){
+	public static void addEffectInteract(PlayerInteractEvent event){
 		Player player = event.getPlayer();
 		// CATCH FIRE
 		if(
-			itemId==259 && Util.pctChance(10) && 
-			player.getWorld().getBlockAt(player.getLocation()).isLiquid()==false && 
-			player.getWorld().hasStorm()==false
+			   event.getItem().getTypeId()==259 && Util.pctChance(10) 
+			&& player.getWorld().getBlockAt(player.getLocation()).isLiquid()==false 
+			&& player.getWorld().hasStorm()==false
 			) {
 			player.setFireTicks(Util.sec2tic(300));
 			effect = 1;
 		}
 		// PUT OUT FIRE
-		else if(itemId==326 && player.getFireTicks()>0) {
+		else if(event.getItem().getTypeId()==326 && player.getFireTicks()>0) {
 			player.setFireTicks(0);
 			event.setCancelled(true);
 			effect = 2;
 		}
 		// CURE FOODPOISON
-		else if(itemId==326 && player.hasPotionEffect(PotionEffectType.POISON)) {
+		else if(event.getItem().getTypeId()==326 && player.hasPotionEffect(PotionEffectType.POISON)) {
 			effect = 4;
 		} 
 		else {
@@ -63,19 +68,6 @@ public class PlayerEffects {
 		if(effect>0) player.sendMessage(effects[effect]);
 	}
 
-	public static void addEffectRegainHealth(String reason, EntityRegainHealthEvent event){
-		Player player = (Player) event.getEntity();
-		// CATCH FIRE
-		if(reason=="EATING" && Util.pctChance(10)) {
-			player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Util.sec2tic(60), 1));
-			effect = 3;
-		} 
-		else {
-			effect = 0;
-		}
-		if(effect>0) player.sendMessage(effects[effect]);
-	}
-	
 	public static void addEffectMove(PlayerMoveEvent event){
 		Player player = event.getPlayer();
 		Location to = event.getTo();
@@ -111,6 +103,40 @@ public class PlayerEffects {
 		}
 		else {
 			effect = 0;
+		}
+		if(effect>0) player.sendMessage(effects[effect]);
+	}
+	
+	public static void addEffectRegainHealth(EntityRegainHealthEvent event){
+		Player player = (Player) event.getEntity();
+		// CATCH FIRE
+		if(event.getRegainReason().equals(RegainReason.EATING) && Util.pctChance(10)) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, Util.sec2tic(60), 1));
+			effect = 3;
+		} 
+		else {
+			effect = 0;
+		}
+		if(effect>0) player.sendMessage(effects[effect]);
+	}
+	
+	public static void addEffectDamage(EntityDamageEvent event){
+		Player player = (Player) event.getEntity();
+		
+		// CUNCUSSION FROM FALL
+		if(event.getCause().equals(DamageCause.FALL)) {
+			
+			if(event.getDamage()>=12) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Util.sec2tic(5), 1));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, Util.sec2tic(15), 1));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Util.sec2tic(10), 1));	
+			} else if(event.getDamage()>=8) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, Util.sec2tic(10), 1));				
+				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, Util.sec2tic(5), 1));	
+			} else if(event.getDamage()>=4){
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, Util.sec2tic(10), 1));				
+			}
+			if(event.getDamage()>=4) effect = 6;
 		}
 		if(effect>0) player.sendMessage(effects[effect]);
 	}
